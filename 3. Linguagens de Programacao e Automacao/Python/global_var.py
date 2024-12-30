6d91cc041276f3b01f34ea6e95fa93dcb36f4fe5
Anna Luisa Sá <91437093+Annalu-Sa@users.noreply.github.com>
2024-12-30 10:12:15 -0300

    Add files via upload

---
+++ 3. Linguagens de Programacao e Automacao/Python/global_var.py
@@ -0,0 +1,14 @@
+x = 'awesome'
+def myfunc():
+  x = 'fantastic'
+myfunc()
+print('Python is ' + x) #aq retorna Python is awsome
+
+
+
+#x = 'awesome'
+#def myfunc():
+#    x = 'fantastic'  # Variável local
+#    return x         # Retorna o valor local
+#local_x = myfunc() # Capturamos o valor local retornado pela função
+#print('Python is ' + local_x) #aq retorna Python is fantastic
